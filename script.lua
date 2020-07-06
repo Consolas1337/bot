53c68e44aecc922e9c8aafd4b18155f2fd4ddd7e
dangerous3225 <danya3225@gmail.com>
2020-07-07 02:51:43 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -53,26 +53,38 @@ end
 findAround(15)]]--
 
 function findAround(range)
-   for x, guid in pairs(lb.GetObjects(range)) do
+   for x, guid in pairs(lb.GetObjects(range,5)) do
       local type, zero, server_id, instance_id, zone_uid, npc_id, spawn_uid = strsplit("-",guid)
       local playerPosX, playerPosY, playerPosZ = lb.ObjectPosition("player")
-      local x1, y1, z1 = __LB__.ObjectPosition(guid)
+      local x1, y1, z1 = lb.ObjectPosition(guid)
       local distance = lb.GetDistance3D(playerPosX, playerPosY, playerPosZ, x1, y1, z1)
-      if (lb.UnitIsLootable(guid) and UnitIsDead(guid))then
+ 
+
+      if not(UnitIsDead(guid)) then
+         print(x, type, npc_id, lb.ObjectType(guid), x1,y1,z1)
+         TargetUnit(guid)
+         lb.Unlock(TargetUnit, guid)
+       --  lb.Unlock(CastSpellByName, "Frostbolt")
+      end
+
+      if (lb.UnitIsLootable(guid) and UnitIsDead(guid)) then
          if (distance > 2) then
             lb.Navigator.MoveTo(x1, y1, z1, 1)
          else
-            Lb.ObjectInteract(guid)
+            TargetUnit(guid)
+            lb.ObjectInteract(guid)
          end
       --print(type, npc_id, lb.ObjectType(guid), x1,y1,z1)
       end
+
+      
 
       --[[if lb.UnitIsLootable(guid) then
          print("колв-во предметов у " , npc_id , GetNumLootItems())
       end]]--
    end
 end
-findAround(20)
+findAround(50)
 --[[SomeFrame = CreateFrame("Frame", "SomeFrame", nil)
 SomeFrame:SetScript("OnUpdate", findAround)]]--
 
